f74351b3f993c628377d9e328bfca3eeee78ea35
Amelia Dutta <amelia280111dutta@gmail.com>
2024-11-25 21:52:42 +0530

    added submit

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import he from "he";
+import geto from "../img/submit.png"
+import gojo from "../img/gojo.png"
 export default function Quiz() {
     const [questions, setQuestions] = useState([])
     //const [error, setError] = useState(false);
@@ -34,9 +36,34 @@ export default function Quiz() {
                 {questions.map((item, index) => (
                     <li key={index}>
                         <h2>{he.decode(item.question)}</h2>
+                        <ul className="answer">
+                            {[
+                                ...item.incorrect_answers,
+                                item.correct_answer,
+                            ]
+                                .sort(() => Math.random() - 0.5)
+                                .map((answer, i) => (
+                                    <div className="options" key={i}>
+                                        <input
+                                            type="radio"
+                                            id={`q${index}_a${i}`} // Unique id per question and answer
+                                            name={`question_${index}`} // Group inputs by question
+                                            value={answer}
+                                        />
+                                        <label htmlFor={`q${index}_a${i}`}>
+                                            {he.decode(answer)}
+                                        </label>
+                                    </div>
+                                ))}
+                        </ul>
                     </li>
                 ))}
                 </ul>
+                <div className="submit">
+                    <img className="sbmit-img1" src={gojo} alt="submit" />
+                    <button className="submit-btn">Submit</button>
+                    <img className="sbmit-img2" src={geto} alt="submit" />
+                </div>
         </section>
     );
 }
